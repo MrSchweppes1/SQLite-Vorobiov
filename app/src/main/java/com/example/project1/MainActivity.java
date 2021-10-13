a1163c3745253b944b84d793db594fd2d64c4076
USER <USER@DESKTOP-GUL41FM>
2021-10-13 21:16:17 +0300

    Почти сделал, кроме дз 13.10.2021

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -8,8 +8,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -19,7 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView tvText;
     DBHelper dbHelper;
     String str;
+    SQLiteDatabase database;
 
+    ContentValues contentValues;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,64 +46,118 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+
+        UpdateTable();
+    }
+
+    public void UpdateTable(){
+        Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+        tvText.setText("");
+        str= null;
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+            int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
+            TableLayout dbOutput = findViewById(R.id.dbOutput);
+            dbOutput.removeAllViews();
+            do {
+                TableRow dbOutputRow = new TableRow(this);
+                dbOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+
+                LinearLayout.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT);
+
+
+                TextView outputID = new TextView(this);
+                params.weight = 1.0f;
+                outputID.setLayoutParams(params);
+                outputID.setText(cursor.getString(idIndex));
+                dbOutputRow.addView(outputID);
+
+                TextView outputName = new TextView(this);
+                params.weight = 3.0f;
+                outputName.setLayoutParams(params);
+                outputName.setText(cursor.getString(nameIndex));
+                dbOutputRow.addView(outputName);
+
+                TextView outputMail = new TextView(this);
+                params.weight = 3.0f;
+                outputMail.setLayoutParams(params);
+                outputMail.setText(cursor.getString(emailIndex));
+                dbOutputRow.addView(outputMail);
+
+                Button deleteBtn = new Button(this);
+                deleteBtn.setOnClickListener(this);
+                params.weight = 1.0f;
+                deleteBtn.setLayoutParams(params);
+                deleteBtn.setText("Удалить запись");
+                deleteBtn.setId(cursor.getInt(idIndex));
+                dbOutputRow.addView(deleteBtn);
+
+                dbOutputRow.addView(dbOutputRow);
+
+
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
     }
 
     @Override
     public void onClick(View v) {
 
-        String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
-
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-
-
         switch (v.getId()) {
 
             case R.id.Add:
+
+                String name = etName.getText().toString();
+                String email = etEmail.getText().toString();
+
+                contentValues = new ContentValues();
+
                 contentValues.put(DBHelper.KEY_NAME, name);
                 contentValues.put(DBHelper.KEY_MAIL, email);
 
                 database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
-                break;
-
-            case R.id.Read:
-                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-                tvText.setText("");
-                str= null;
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    do {
-                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex));
-                    } while (cursor.moveToNext());
-                } else
-                    Log.d("mLog", "0 rows");
-
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    do {
-                        str+=("ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex) + "\n");
-                    } while (cursor.moveToNext());
-                    tvText.setText(str.toString());
-                } else
-                    tvText.setText("В базе пусто" );
-
-                cursor.close();
+                UpdateTable();
                 break;
 
             case R.id.Clear:
                 database.delete(DBHelper.TABLE_CONTACTS, null, null);
-                tvText.setText("");
-                str= null;
+
+                TableLayout dbOutput = findViewById(R.id.dbOutput);
+                dbOutput.removeAllViews();
+                UpdateTable();
+                break;
+
+            default:
+                View outputDBRow = (View) v.getParent();
+                ViewGroup outputDB = (ViewGroup) outputDBRow.getParent();
+                outputDB.removeView(outputDBRow);
+                outputDB.invalidate();
+
+                database.delete(DBHelper.TABLE_CONTACTS, DBHelper.KEY_ID + " = ?", new String[]{String.valueOf((v.getId()))});
+
+                contentValues = new ContentValues();
+                Cursor cursorUpdater = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+                if (cursorUpdater.moveToFirst()) {
+                    int idIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_NAME);
+                    int emailIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_MAIL);
+                    int realID = 1;
+                    do {
+                        if (cursorUpdater.getInt(idIndex)>realID){
+                            contentValues.put(DBHelper.KEY_ID,realID);
+                            contentValues.put(DBHelper.KEY_NAME,cursorUpdater.getString(nameIndex));
+                            contentValues.put(DBHelper.KEY_MAIL,cursorUpdater.getString(emailIndex));
+                            database.replace(DBHelper.TABLE_CONTACTS,null,contentValues);
+                        }
+                        realID++;
+                    }while (cursorUpdater.moveToNext());
+                    if (cursorUpdater.moveToLast()){
+                        database.delete(DBHelper.TABLE_CONTACTS, DBHelper.KEY_ID + " = ?", new String[]{cursorUpdater.getString(idIndex)});
+                    }
+                    UpdateTable();
+                }
                 break;
 
         }
